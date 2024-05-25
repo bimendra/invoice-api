052e674e20d37541eed25b4c5608339391140160
Bimendra Bandara <bimendra.85@gmail.com>
2024-05-25 12:17:53 +0800

    Customer/Invoice end points

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\CustomerController;
-use App\Http\Controllers\Api\V1\InvoiceController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
